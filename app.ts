29cadc523dd95c415a074fc0e2169222ac8dc8dd
David Guras <gura.san@gmail.com>
2022-01-02 22:26:42 -0500

    Change file names
    - rename app.ts to basics.ts
    - create new app.ts file
    - add ts object example to app.ts

--- app.ts
+++ app.ts
@@ -1,19 +1,9 @@
-// In TypeScript, you work with types like string or number all the times.
-// Important: It is string and number (etc.), NOT String, Number etc.
-// The core primitive types in TypeScript are all lowercase!
+const person: {
+  name: string;
+  age: number;
+} = {
+  name: "Max",
+  age: 30,
+};
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "Result is: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
+console.log(person.name);
---
+++ basics.ts
@@ -0,0 +1,19 @@
+// In TypeScript, you work with types like string or number all the times.
+// Important: It is string and number (etc.), NOT String, Number etc.
+// The core primitive types in TypeScript are all lowercase!
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
+  }
+}
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+const resultPhrase = "Result is: ";
+
+const result = add(number1, number2, printResult, resultPhrase);
