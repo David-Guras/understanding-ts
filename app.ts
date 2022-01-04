b0725fce8de372acb68f266109b9d95c1c0c713f
David Guras <gura.san@gmail.com>
2022-01-03 21:49:21 -0500

    Explore tuple type in TS

--- app.ts
+++ app.ts
@@ -1,12 +1,16 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-const person = {
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
   name: "Max",
   age: 30,
   hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
+
+person.role.push("admin"); // tuples allow for push operation
 
 let favouriteAcitivities: string[];
 favouriteAcitivities = ["Sports", "Cooking"];
@@ -16,5 +20,4 @@ console.log(person.name);
 for (const hobby of person.hobbies) {
   console.log(hobby.toLocaleUpperCase());
   // console.log(hobby.map()); // !!! ERROR !!!
-  
 }
