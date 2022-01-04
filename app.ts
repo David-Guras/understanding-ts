cde6891ed78bcbda9adfcd5e4bde60ba1688b564
David Guras <gura.san@gmail.com>
2022-01-03 21:42:09 -0500

    Explore array type in TS

--- app.ts
+++ app.ts
@@ -1,9 +1,20 @@
-const person: {
-  name: string;
-  age: number;
-} = {
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
   name: "Max",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favouriteAcitivities: string[];
+favouriteAcitivities = ["Sports", "Cooking"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toLocaleUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+  
+}
