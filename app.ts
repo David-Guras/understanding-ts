d1e7200ee09d061fdd5e3ea38ccbc745cf598a6d
David Guras <gura.san@gmail.com>
2022-01-04 23:12:06 -0500

    Explore function type in TS

--- app.ts
+++ app.ts
@@ -6,4 +6,14 @@ function printResults(num: number): void {
   console.log("Result " + num);
 }
 
+let combinedValues: (a: number, b:number) => number;
+
+combinedValues = add;
+// combinedValues = printResult; // !!! Error !!!
+
+combinedValues = add;
+
+console.log(combinedValues(8, 8));
+
+
 // let someValue: undefined;
