df9cc1d9515bf0f30df46694791d69af187f7a02
David Guras <gura.san@gmail.com>
2022-01-04 23:06:49 -0500

    Explore function return types in TS

--- app.ts
+++ app.ts
@@ -1,30 +1,9 @@
-type Conbinable = number | string;
-type ConversionDescriptor = "as-number" | "as-text";
-
-function combine(
-  input1: Conbinable,
-  input2: Conbinable,
-  resultConversion: ConversionDescriptor // Example of Literal type
-) {
-  let result;
-
-  if (typeof input1 === "number" && typeof input2 === "number") {
-    result = input1 + input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-  if (resultConversion === "as-number") {
-    return +result;
-  } else {
-    return result.toString();
-  }
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
-console.log(combinedAges);
+function printResults(num: number): void {
+  console.log("Result " + num);
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
-
-const combinedNames = combine("Max", "Anna", "as-text");
-console.log(combinedNames);
+// let someValue: undefined;
