da5357421bc3b828afc3cb06ca716833a3315289
David Guras <gura.san@gmail.com>
2022-01-05 21:47:54 -0500

    Explore callback functions in TS

--- app.ts
+++ app.ts
@@ -6,7 +6,12 @@ function printResults(num: number): void {
   console.log("Result " + num);
 }
 
-let combinedValues: (a: number, b:number) => number;
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  cb(result);
+}
+
+let combinedValues: (a: number, b: number) => number;
 
 combinedValues = add;
 // combinedValues = printResult; // !!! Error !!!
@@ -15,5 +20,8 @@ combinedValues = add;
 
 console.log(combinedValues(8, 8));
 
-
 // let someValue: undefined;
+
+addAndHandle(10, 20, (result) => {
+  console.log(result);
+});
