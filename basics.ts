9952a2d8ed6e39288c87cef042ff25f2c77056d1
David Guras <gura.san@gmail.com>
2022-01-05 22:14:57 -0500

    Explore tsc --init command in TS

--- basics.ts
+++ basics.ts
@@ -2,7 +2,7 @@
 // Important: It is string and number (etc.), NOT String, Number etc.
 // The core primitive types in TypeScript are all lowercase!
 
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+function add1(n1: number, n2: number, showResult: boolean, phrase: string) {
   const result = n1 + n2;
   if (showResult) {
     console.log(phrase + result);
@@ -16,4 +16,4 @@ const number2 = 2.8;
 const printResult = true;
 const resultPhrase = "Result is: ";
 
-const result = add(number1, number2, printResult, resultPhrase);
+const result = add1(number1, number2, printResult, resultPhrase);
