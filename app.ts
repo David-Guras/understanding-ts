2a83370b59f00bb2921969c5e427460cd0d09a6e
David Guras <gura.san@gmail.com>
2022-01-07 00:02:56 -0500

    Explore variation of arrow function in TS

--- app.ts
+++ app.ts
@@ -6,3 +6,10 @@ let age = 35;
 
 //arrow function
 const addStuff = (a: number, b: number) =>  a + b;
+
+//variation of arrow function
+const button = document.querySelector("button");
+
+if (button) {
+    button.addEventListener('click', event => console.log(event));
+}
