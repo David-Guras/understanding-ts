c9aa0101b5f4a50b3ad61e9b7ea6096ee829ba35
David Guras <gura.san@gmail.com>
2022-01-07 00:12:01 -0500

    Explore spread operator in TS

--- app.ts
+++ app.ts
@@ -5,15 +5,28 @@ const anotherName = "David";
 let age = 35;
 
 // arrow function
-const addStuff = (a: number, b: number) =>  a + b;
+const addStuff = (a: number, b: number) => a + b;
 
 // defatul values for parameters, can be called with one argument
 // parameters without default should come first
-const addMoreStuff = (a: number, b: number = 1) =>  a + b;
+const addMoreStuff = (a: number, b: number = 1) => a + b;
 
 // variation of arrow function
 const button = document.querySelector("button");
 
 if (button) {
-    button.addEventListener('click', event => console.log(event));
+  button.addEventListener("click", (event) => console.log(event));
 }
+
+// spread operator
+const hobbies = ["Sports", "Hobbies"];
+const activeHobbies = ["Hiking", ...hobbies];
+
+activeHobbies.push(...hobbies);
+
+// spread operator also exists on objects
+const somePerson = {
+  name: "David",
+  height: "6'3''",
+};
+const copiedPerson = { ...person };
